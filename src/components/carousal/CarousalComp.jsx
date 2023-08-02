b2d02fccec6e8941e24ddec0d5bd26ef37ee2092
Suman Pokhrel <spokhrel196@gmail.com>
2023-08-02 14:46:07 +0545

    carousal logic implemented

--- src/components/carousal/CarousalComp.jsx
+++ src/components/carousal/CarousalComp.jsx
@@ -13,6 +13,8 @@ const CarousalComp = () => {
         dots: true,
         infinite: true,
         speed: 500,
+        autoplay: true,
+        autoplaySpeed: 4000,
         slidesToShow: 1,
         slidesToScroll: 1
     };
