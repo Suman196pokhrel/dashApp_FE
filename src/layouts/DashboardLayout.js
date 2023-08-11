54c535d453fba7ef66e35e10002ada0af062d7f1
Suman Pokhrel <spokhrel196@gmail.com>
2023-08-11 13:08:56 +0545

    upgraded auth routes to protect manual navigation to parent routes like /auth and /dashboard

--- src/layouts/DashboardLayout.js
+++ src/layouts/DashboardLayout.js
@@ -3,10 +3,23 @@ import "../styles/dashboardLayout.scss"
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/sidebar/Sidebar";
 import TopBar from "../components/topBar/TopBar";
-import { motion } from 'framer-motion'
-import { smoothComeUp } from "../utils/framerAnimations";
+import { Link, useLocation } from 'react-router-dom'
+import { motion } from "framer-motion"
+import { smoothComeUp, smoothComeDown, smoothFromRight } from '../utils/framerAnimations'
+import Oops from "../components/Opps/Opps"
 
 const DashLayout = () => {
+
+  let { pathname } = useLocation()
+  const pathname1 = pathname.substring(pathname.indexOf('/'))
+  pathname = pathname.substring(pathname.lastIndexOf('/') + 1)
+  console.log(pathname)
+  console.log(pathname1)
+
+
+
+
+
   return (
     <div className="dashboardLayout"  >
       <Sidebar />
@@ -14,7 +27,12 @@ const DashLayout = () => {
       <div className="children">
         <TopBar />
         <div className="dashApps" >
-          <Outlet />
+          {pathname1 === '/dashboard' ? (
+            <Oops />
+          ) : (
+            <Outlet />
+          )}
+
         </div>
 
 
