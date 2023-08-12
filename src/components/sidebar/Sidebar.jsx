05449be63aae08c5b9ee9753470b7426e90a36c0
Suman Pokhrel <spokhrel196@gmail.com>
2023-08-12 08:28:41 +0545

    completed auth and sidebar responsiveness

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import "./sidebar.scss"
 import { CssBaseline, Drawer } from '@mui/material'
 import IconButton from '@mui/material/IconButton'
@@ -13,28 +13,33 @@ import SvgIcon from '@mui/material/SvgIcon'
 
 const Sidebar = () => {
 
-    const [isDrawerOpen, setIsDrawerOpen] = useState(true)
+    // const [isDrawerOpen, setIsDrawerOpen] = useState(true)
+    // const [currentWidth, setCurrentWidth] = useState(0)
+    // const sidebarRef = useRef(null)
     const { pathname } = useLocation()
     const [currentPath, setCurrentPath] = useState(pathname.substring(pathname.lastIndexOf('/') + 1))
-    const sidebarWidth = isDrawerOpen ? `320px` : '80px'; // Change 80 to your desired width for each nav item
+    // const sidebarWidth = isDrawerOpen ? `320px` : '80px'; // Change 80 to your desired width for each nav item
 
 
-    const handleDrawerOpen = () => {
-        console.log("Open clicked")
-        setIsDrawerOpen(true)
-    }
+    // const handleDrawerOpen = () => {
+    //     console.log("Open clicked")
+    //     setIsDrawerOpen(true)
+    // }
 
-    const handleDrawerClose = () => {
-        console.log("Close clicked")
-        setIsDrawerOpen(false)
-    }
+    // const handleDrawerClose = () => {
+    //     console.log("Close clicked")
+    //     setIsDrawerOpen(false)
+    // }
 
+    useEffect(() => {
+
+    }, [])
 
 
     return (
-        <div className='sidebar' style={{ width: sidebarWidth }}>
+        <div className='sidebar' >
 
-            <div className="drawerControl">
+            {/* <div className="drawerControl">
                 {isDrawerOpen ? (
                     <IconButton onClick={handleDrawerClose}>
                         <KeyboardArrowLeftIcon />
@@ -45,7 +50,7 @@ const Sidebar = () => {
                     </IconButton>
                 )}
 
-            </div>
+            </div> */}
 
             <div className="logoContainer">
                 <img src="/favicon/android-chrome-192x192.png" alt="brang logo" />
@@ -53,24 +58,24 @@ const Sidebar = () => {
 
             <div className="adminTab">
                 <img src="/static/mockPhotos/suman.jpeg" alt="AdminPic" />
-                {isDrawerOpen ? (<div className="adminInfo">
+                <div className="adminInfo">
                     <p>Suman Pokhrel</p>
                     <span>Admin</span>
-                </div>) : (<></>)}
+                </div>
             </div>
 
             {sidebarItems.map((topic) => (
-                <div className="topic" key={topic.id} style={isDrawerOpen ? {} : { paddingLeft: "0px" }}>
-                    {isDrawerOpen ? (<h1>{topic.title}</h1>) : (<></>)}
+                <div className="topic" key={topic.id}>
+                    <h1>{topic.title}</h1>
                     {topic.listItems.map((item) => (
                         <Link key={item.id} to={item.url} onClick={() => {
                             setCurrentPath(item.url);
                             // console.log(currentPath)
                         }}>
-                            <div className={`item ${currentPath === item.url ? "selectedLink" : ""}`} style={isDrawerOpen ? { flexDirection: "row" } : { flexDirection: "column", height: "70px", backgroundColor: "", alignItems: "center", justifyContent: "center", }}>
+                            <div className={`item ${currentPath === item.url ? "selectedLink" : ""}`}>
                                 {<>{item.icon}</>}
                                 {/* <img src={item.icon} alt={item.title} /> */}
-                                {isDrawerOpen ? (<p>{item.title}</p>) : (<p style={{ fontSize: "12px", margin: "0px" }}>{item.title}</p>)}
+                                <p>{item.title}</p>
                             </div>
                         </Link>
 
